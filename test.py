e154ff390b3be2b646466e13531ba81083d2ab94
TongyJia <jty@emails.bjut.edu.cn>
2021-08-24 16:31:06 +0800

    Add files via upload

--- test.py
+++ test.py
@@ -23,15 +23,11 @@ from skimage import transform
 parser = argparse.ArgumentParser(description='Operation-wise Attention Network')
 parser.add_argument('--gpu_num', '-g', type=int, default=0, help='Num. of GPUs')
 parser.add_argument('--mode', '-m', default='yourdata', help='Mode (mix / yourdata)')
-#parser.add_argument('--dehazemodel', type=str,default='./model-dual10/dehaze_100.pth', help='transform model file to use')
-#parser.add_argument('--metamodel', type=str,default='./model1230/meta_20.pth', help='meta model file to use')
-#parser.add_argument('--dehazemodel', type=str,default='./model_1.pth', help='transform model file to use')
 
 
 args = parser.parse_args()
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-#model = torch.load(args.dehazemodel).to(device)
 # load dataset
 if args.mode == 'mix' or args.mode == 'yourdata':
     if args.mode == 'mix':
@@ -39,37 +35,19 @@ if args.mode == 'mix' or args.mode == 'yourdata':
         train_dir = '/dataset/train/'
         val_dir = '/dataset/val/'
         test_dir = './dataset/test/'
-        #test_set = get_dataset_deform(train_dir, val_dir, test_dir, 2)
-        #test_dataloader = DataLoader(dataset=test_set, num_workers=num_work, batch_size=1, shuffle=False, pin_memory=False)
     elif args.mode == 'yourdata':
         num_work = 1
-        #test_input_dir = '/home/omnisky/volume/RESIDE/sots/nyu500/hazy'#'/home/omnisky/volume/RESIDE/sots/nyu500/hazy'
-        #test_target_dir = '/home/omnisky/volume/RESIDE/sots/nyu500/gt'#'/home/omnisky/volume/RESIDE/sots/nyu500/gt'
-        #/home/ubuntu/4meta-dual/DualResidualNetworks-master
-        test_set = TestDatasetFromFolder4('/home/omnisky/4t/RESIDE/RTTS/RTTS')# /home/omnisky/4t/RESIDE/RTTS/RTTS    #/home/omnisky/4t/JTY/J/data/test-rrrr   ##./mytest_A  #/home/omnisky/4t/JTY/testdataset/mytest_A
-        #test_set = TestDatasetFromFolder1('/home/omnisky/volume/0dehaze-project/GridDehazeNet-master/data/test/SOTS/indoor')#('/home/omnisky/volume/RESIDE/sots/outdoor')#nyu500 outdoor
-        # test_set = TestDatasetFromFolder2('/home/omnisky/volume/0dehaze-project/GridDehazeNet-master/data/test/SOTS/outdoor')
-        #test_set = TestDatasetFromFolder1('./result')
-        #test_set = get_training_set(test_input_dir, test_target_dir, False)
+        test_set = TestDatasetFromFolder4('/home/omnisky/4t/RESIDE/RTTS/RTTS')#
         test_dataloader = DataLoader(dataset=test_set, num_workers=num_work, batch_size=1, shuffle=False, pin_memory=False)
         test_bar = tqdm(test_dataloader, desc='[testing datasets]')
 else:
     print('\tInvalid input dataset name at CNN_train()')
     exit(1)
 
-#meta = torch.load("./model/dehaze_80.pth",map_location="cuda:0")
-#metanet = torch.load(args.metamodel,map_location="cuda:0")#.to(device)
-#dehaze_net = torch.load(args.dehazemodel,map_location="cuda:0")#.to(device)
 prepro=pre(0.5,0)
 meta = torch.load("./dehaze_80.pth",map_location="cuda:0")#,map_location="cuda:0"
-print("meta have {} parameters in total".format(sum(x.numel() for x in meta.parameters())))
-
-
-model = TransformNet(32)#.to(device)
-
+model = TransformNet(32)
 model.load_state_dict(torch.load('./model.pth'))
-
-print("net have {} parameters in total".format(sum(x.numel() for x in model.parameters())))
 model = model.cuda(0)
 
 # model
@@ -79,11 +57,7 @@ torch.cuda.manual_seed(2018)
 torch.backends.cudnn.benchmark = True
 torch.backends.cudnn.enabled = True
 
-#model = model.cuda(gpuID)
-#logging.info("param size = %fMB", utils.count_parameters_in_MB(model))
-#print('Param:', utils.count_parameters_in_MB(model))
-# for results
-if not os.path.exists('./results_r'):
+if not os.path.exists('./results_r'):  #save path
     os.makedirs('./results_r/Inputs')
     os.makedirs('./results_r/Outputs')
     os.makedirs('./results_r/Targets')
@@ -96,18 +70,12 @@ eps = 1e-10
 start_time = time.time()
 tt = 0
 
-#for i, (input, target) in enumerate(test_dataloader):
 for image_name, input, target in test_bar:
     #print(input.size())
     #print(target.size())
     image_name = image_name[0]
     lr_patch = Variable(input, requires_grad=False).cuda(gpuID)
     hr_patch = Variable(target, requires_grad=False).cuda(gpuID)
-    #output = model(lr_patch)
-
-    #lr_patch = Variable(input, requires_grad=False).cuda(gpuID)
-   # hr_patch = Variable(target, requires_grad=False).cuda(gpuID)
-    # output = model(lr_patch)
 
     unloader = transforms.ToPILImage()
     image = lr_patch.cpu().clone()
@@ -127,9 +95,8 @@ for image_name, input, target in test_bar:
     print(str((t1 - t0)))
     # save images
     vutils.save_image(output.data, './results_r/Outputs/' + image_name, padding=0, normalize=True)
-    #vutils.save_image(output.data, './results_rr/Outputs/%05d.png' % (int(i)), padding=0, normalize=True)#False
-    #vutils.save_image(lr_patch.data, './results_rr/Inputs/%05d.png' % (int(i)), padding=0, normalize=True)
-   # vutils.save_image(hr_patch.data, './results_rr/Targets/%05d.png' % (int(i)), padding=0, normalize=True)
+    vutils.save_image(lr_patch.data, './results_r/Inputs/' + image_name, padding=0, normalize=True)
+    vutils.save_image(hr_patch.data, './results_r/Targets/'+ image_name, padding=0, normalize=True)
     # SSIM and PSNR
     #print(output)
     output = output.data.cpu().numpy()[0]
@@ -143,13 +110,13 @@ for image_name, input, target in test_bar:
     hr_patch = hr_patch.transpose((1,2,0))
     # SSIM
     ssim = ski_ssim(output, hr_patch, data_range=1, multichannel=True)
-    test_ssim+=ssim #ski_ssim(output, hr_patch, data_range=1, multichannel=True)
+    test_ssim+=ssim
     # PSNR
     imdf = (output - hr_patch) ** 2
     mse = np.mean(imdf) + eps
     psnr = 10 * math.log10(1.0/mse)
 
-    test_psnr+= psnr#10 * math.log10(1.0/mse)
+    test_psnr+= psnr
     test_ite += 1
     print('PSNR: {:.4f}'.format(psnr))
     print('SSIM: {:.4f}'.format(ssim))
@@ -157,7 +124,6 @@ for image_name, input, target in test_bar:
 print('mt:', test_ite, tt/test_ite)
 test_psnr /= (test_ite)
 test_ssim /= (test_ite)
-#print('time ', time.time() - start_time)
 print('Test mPSNR: {:.4f}'.format(test_psnr))
 print('Test mSSIM: {:.4f}'.format(test_ssim))
 print('------------------------')
